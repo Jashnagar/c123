7aae345a7e0798af602960f9d3040041591f1570
Jashnagar <126826939+Jashnagar@users.noreply.github.com>
2024-01-18 18:04:48 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup()
 {
    video = createCapture(VIDEO);
@@ -12,6 +15,10 @@ function setup()
 function draw()
 {
     background('#5B4EFF');
+    textSize(difference);
+    stroke('black');
+    fill('white')
+    text('Hello World', 50 , 250);
 }
 function modelLoaded()
 {
@@ -23,5 +30,9 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference =floor(leftWristX - rightWristX);
+        console.log("leftwristx"+leftWristX+"rightwristx"+rightWristX+"difference"+difference);
     }
 }
